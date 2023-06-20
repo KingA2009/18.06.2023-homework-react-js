2da549d5c23e05f317a08af101649187b0ecd364
KingA2009 <ajamoliddinov12@gmail.com>
2023-06-20 18:23:36 +0500

    homework

---
+++ src/components/Nav.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div className='container flex shadow-lg shadow-gray-500 w-[80%] justify-between m-auto mt-8 bg-red-100 rounded-lg'>
+            <div className='border-red-500 border-solid  border-2 w-[30%] rounded-lg mt-4 mb-3 ml-3 mr-3 hover:bg-red-200 scale-100 duration-300'>
+            <div className='mb-3'>
+                <img className='rounded-lg' src="https://media.istockphoto.com/id/1059661424/photo/mature-mixed-race-business-man.jpg?s=612x612&w=0&k=20&c=UAVBeyoD_LkCh1MzVaWW1SR1iwK-VkPDXWMH2o2wL8M=" alt="" />
+            </div>
+            <div className='text-xl max-w-prose justify-center text-center'>
+                <h1 class="font-Kablammo text-red-400">Asadbek Jamoliddinov</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, consequatur?</p>
+                <div className='mt-3 justify-center '>
+                <button className='text-center bg-red-300 rounded-lg p-2 mb-3'>Click Me</button>
+            </div>
+            </div>
+            </div>
+            <div className='border-red-500 border-solid  border-2 w-[30%] rounded-lg mt-4 mb-3 ml-3 mr-3 hover:bg-red-200 scale-100 duration-300'>
+            <div className='mb-3'>
+                <img className='rounded-lg' src="https://media.istockphoto.com/id/1059661424/photo/mature-mixed-race-business-man.jpg?s=612x612&w=0&k=20&c=UAVBeyoD_LkCh1MzVaWW1SR1iwK-VkPDXWMH2o2wL8M=" alt="" />
+            </div>
+            <div className='text-xl max-w-prose justify-center text-center'>
+                <h1 class="font-Kablammo text-red-400">Asadbek Jamoliddinov</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, consequatur?</p>
+                <div className='mt-3 justify-center '>
+                <button className='text-center bg-red-300 rounded-lg p-2 mb-3'>Click Me</button>
+            </div>
+            </div>
+            </div>
+            <div className='border-red-500 border-solid  border-2 w-[30%] rounded-lg mt-4 mb-3 ml-3 mr-3 hover:bg-red-200 scale-100 duration-300'>
+            <div className='mb-3'>
+                <img className='rounded-lg' src="https://media.istockphoto.com/id/1059661424/photo/mature-mixed-race-business-man.jpg?s=612x612&w=0&k=20&c=UAVBeyoD_LkCh1MzVaWW1SR1iwK-VkPDXWMH2o2wL8M=" alt="" />
+            </div>
+            <div className='text-xl max-w-prose justify-center text-center'>
+                <h1 class="font-Kablammo text-red-400">Asadbek Jamoliddinov</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, consequatur?</p>
+                <div className='mt-3 justify-center '>
+                <button className='text-center bg-red-300 rounded-lg p-2 mb-3'>Click Me</button>
+            </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
